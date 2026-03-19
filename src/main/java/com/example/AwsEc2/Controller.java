6037c22c7c923ba1691010e6e07d799135742a14
TornovDutta <tornovdutta@gmail.com>
2026-03-19 15:08:40 +0530

    add the cross origin

--- src/main/java/com/example/AwsEc2/Controller.java
+++ src/main/java/com/example/AwsEc2/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 public class Controller {
 
     @Value("${gemini.api.key}")
